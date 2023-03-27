1c6fde560a41bc2894ace2e4bed06f3b8f4f43e3
Josh <108093283+bigbootylatinas@users.noreply.github.com>
2023-03-27 18:36:02 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,15 +9,15 @@ var _app = function () {
     this.shouldIgnoreVideo = false;
     this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
     this.brandDescription = [
-        "made by a retard",
-        "made for retards",
-        "made with retardation",
+        "made by a exploiter",
+        "made for exploiter",
+        "made with exploits",
         "simply the best"
     ];
 
     this.titleChanger = function (text, delay) {
         text = text ||
-            ['a', 'an', 'ant', 'anti', 'antis', 'antiso', 'antisoz', 'antisozi', 'antisozia', 'antisozial', 'antisozial.', 'antisozial.t', 'antisozial.te', 'antisozial.tea', 'antisozial.team', 'ntisozial.team', 'tisozial.team', 'isozial.team', 'sozial.team', 'ozial.team', 'zial.team', 'ial.team', 'al.team', 'l.team', '.team', 'team', 'eam', 'am', 'm', '⠀⠀'];
+            ['m', 'me', 'mew', 'mewh', 'mewhu', 'mewhub', '⠀⠀'];
         delay = delay || 800;
 
         var counter = 0;
